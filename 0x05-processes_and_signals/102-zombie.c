9758b83aa67395ab9a08f9e838c54333f75a1148
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-07-01 22:59:53 +0400

    up

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * infinite_while - check the code
+ * Return: zero but it will not get to that
+ */
+int infinite_while(void)
+{
+    while (1)
+    {
+        sleep(1);
+    }
+    return (0);
+}
+
+
+/**
+ * main - check the code
+ * Return: Nothing
+ */
+int main()
+{
+	int	pid;
+	int i = 0;
+
+	pid = fork();
+	while (i++ < 4)
+		if (pid)
+			pid = fork();
+	if (!pid)
+	{
+		printf("Zombie process created, PID: %d\n", getpid());
+		infinite_while();
+	}
+	return 0;
+}
